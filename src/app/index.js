87302dc698f5e0d56ed8c10ac57714788e4261da
Louizhou <1435676096@qq.com>
2019-08-01 15:18:41 +0800

    create a weather app

---
+++ src/app/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import HomePage from './screens/Home'
+
+class App extends React.Component{
+    render(){
+         return <HomePage/>
+    }
+}
+
+export default App
---
+++ src/app/screens/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+class HomePage extends React.Component{
+    render(){
+         return <div>Hello Home</div>
+    }
+}
+
+export default HomePage
